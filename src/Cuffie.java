7b6e31ed50fb4cf931bae391df21684bb36906fb
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-03-01 12:23:30 +0100

    add

--- src/Cuffie.java
+++ src/Cuffie.java
@@ -1,7 +1,12 @@
 public class Cuffie extends Prodotto {
-
-    public Cuffie(int code, String name, String brand, double price, int iva) {
+    String colour;
+    boolean isWireless;
+    public Cuffie(int code, String name, String brand, double price, int iva, String colour, boolean isWireless) {
         super(code, name, brand, price, iva);
+        this.colour=colour;
+        this.isWireless=isWireless;
     }
 
+
 }
+
--- src/Televisori.java
+++ src/Televisori.java
@@ -1,7 +1,23 @@
 public class Televisori extends Prodotto{
-
-    public Televisori(int code, String name, String brand, double price, int iva) {
+    double size;
+    boolean isSmart;
+    public Televisori(int code, String name, String brand, double price, int iva, double size, boolean isSmart) {
         super(code, name, brand, price, iva);
+        this.size=size;
+        this.isSmart=isSmart;
     }
+    public double getSize() {
+        return size;
+    }
+    public void setSize(double size) {
+        this.size = size;
+    }
+    public boolean isSmart() {
+        return isSmart;
+    }
+    public void setSmart(boolean isSmart) {
+        this.isSmart = isSmart;
+    }
+    
 
 }
